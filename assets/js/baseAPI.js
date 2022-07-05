5f0d415a372520f3528a3f4f2ed54a2f4db219fe
guoshuaikang <1029895026@qq.com>
2022-07-05 21:27:59 +0800

    完成主页开发功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,8 +1,26 @@
 // 每次调用 $.get()或$.post()或$.ajax()的时候
 // 会先调用ajaxPrefilter 函数
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
-$.ajaxPrefilter(function(options){
+$.ajaxPrefilter(function (options) {
 
-options.url = 'http://www.liulongbin.top:3007' + options.url
-console.log(options.url);
+    options.url = 'http://www.liulongbin.top:3007' + options.url
+    // console.log(options.url);
+
+    // 统一为有权限的接口设置headers请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    // 全局统一挂载 complete 回调函数
+
+    options.complete = function(res) {
+         // 判断用户是否登录
+         if(res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！'){
+            //    强制清空 token
+                localStorage.removeItem('token')
+                // 强制跳转至登录页面
+                location.href='/login.html'
+            }
+    }
 })
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,63 @@
-$(function() {
-    
+
+$(function () {
+    // 调用 getUserInfo 获取用户基本信息
+    getUserInfo()
+    //    退出点击事件
+    var layer =layui.layer
+    $('#btnLogout').on('click', function () {
+        // 提示用户是否确认推出
+        layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function (index) {
+           
+            // 清空本地存储中 token
+            localStorage.removeItem('token')
+            // 跳转到登录页
+            location.href='/login.html'
+            // 关闭 cofirm 询问框
+            layer.close(index)
+        });
+    })
+
+
+
 })
+// 获取用户的基本信息
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        // headers就是请求头配置对象
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
+        success: function (res) {
+            if (res.status !== 0) {
+                return console.log(res.message)
+
+            }
+            // 调用 renderAvatar 渲染用户的头像
+            renderAvatar(res.data)
+            console.log(res);
+        }
+        
+
+    })
+}
+// 渲染用户头像
+function renderAvatar(user) {
+    //  设置用户的名称
+    var name = user.nickname || user.username
+    // 设置欢迎的文本
+    $('#welcome').html('欢迎&nbsp&nbsp' + name)
+    // 判断是否又图片头像进行渲染头像
+    if (user.user_pic !== null) {
+        // 渲染图片头像
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        $('.text-avatar').hide()
+
+    } else {
+        let first = name[0].toUpperCase()
+        $('.layui-nav-img').hide()
+        $('.text-avatar').html(first).show()
+    }
+
+}
